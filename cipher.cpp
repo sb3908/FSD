fc725977b22a9ebc15383053b075c8e923734ed6
sb3908 <141304835+sb3908@users.noreply.github.com>
2023-08-10 14:56:04 +0530

    Update cipher.cpp

--- cipher.cpp
+++ cipher.cpp
@@ -1,50 +1,54 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-string encrypt(string text,int shift)
-{
-    string result="";
-    for(int i=0;i<text.length();i++){
-       char ch=text[i];
-       int new_ch=ch+shift;
-       if(new_ch>'Z'){
-
-
-        new_ch=new_ch-26;
-       }
-        else if(new_ch<'A'){
-
-            new_ch=new_ch+26;
-                 }
-      result += new_ch;
+string encrypt(string text, int shift) {
+    string result = "";
+    for (int i = 0; i < text.length(); i++) {
+        char ch = text[i];
+        if (isalpha(ch)) {
+            int new_ch = ch + shift;
+            if (isupper(ch)) {
+                if (new_ch > 'Z') {
+                    new_ch = new_ch - 26;
+                } else if (new_ch < 'A') {
+                    new_ch = new_ch + 26;
+                }
+            } else if (islower(ch)) {
+                if (new_ch > 'z') {
+                    new_ch = new_ch - 26;
+                } else if (new_ch < 'a') {
+                    new_ch = new_ch + 26;
+                }
+            }
+            result += new_ch;
+        } else {
+            result += ch;  
+        }
     }
-
-
     return result;
-
-}
-string decrypt(string text,int shift){
- shift=26-shift;
- return  decrypt(text,shift);
-
-}
-int main(){
-string text;
-int shift;
-cout<<"enter the text you want to encrypt"<<endl;
-getline(cin,text);
-cout<<"enter the shift";
-cin>>shift;
-string ciphertext=encrypt(text,shift);
-cout<<"\nciphertext:"<<ciphertext<<endl;
-string desrypt=decrypt(ciphertext,shift);
-cout<<"\ndesrypttext:"<<desrypt<<endl;
-return 0;
 }
 
+string decrypt(string text, int shift) {
+    shift = 26 - shift;
+    return encrypt(text, shift);
+}
 
+int main() {
+    string text;
+    int shift;
+    cout << "Enter the text you want to encrypt:" << endl;
+    getline(cin, text);
+    cout << "Enter the key: ";
+    cin >> shift;
+    cin.ignore(); 
 
+    string ciphertext = encrypt(text, shift);
+    cout << "\nCiphertext: " << ciphertext << endl;
 
+    string decryptedText = decrypt(ciphertext, shift);
+    cout << "\nDecrypted Text: " << decryptedText << endl;
 
+    return 0;
+}
 
